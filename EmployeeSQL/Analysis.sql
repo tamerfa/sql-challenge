927e18644fe23143525a2414cb08695c5f4d39ad
Tamer Abdelaal <tamerfa@gmail.com>
2021-10-05 09:55:41 +1100

    Finished Data Analysis

--- EmployeeSQL/Analysis.sql
+++ EmployeeSQL/Analysis.sql
@@ -1,4 +1,4 @@
--- 1. List employee number, last name, first name, sex, and salary.
+--1. List employee number, last name, first name, sex, and salary.
 SELECT emp_no, last_name, first_name, sex, (
 	SELECT s.salary
 	FROM salaries AS s
@@ -6,7 +6,7 @@ SELECT emp_no, last_name, first_name, sex, (
 	) 
 FROM employees AS e;
 
--- 2. List first name, last name, and hire date for employees who were hired in 1986
+--2. List first name, last name, and hire date for employees who were hired in 1986
 SELECT first_name, last_name, hire_date 
 FROM employees
 WHERE hire_date LIKE '%/1986';
@@ -16,7 +16,7 @@ SELECT * FROM dept_manager;
 SELECT * FROM departments;
 SELECT * FROM employees;
 
--- 3. For each manager, list: department number, department name, employee number, last name, first name
+--3. For each manager, list: department number, department name, employee number, last name, first name
 SELECT d.dept_no AS "Department No.", 
 	   d.dept_name AS "Department Name",
 	   e.emp_no AS "Employee No.",
@@ -28,21 +28,9 @@ ON dm.dept_no = d.dept_no
 LEFT JOIN employees AS e
 ON dm.emp_no = e.emp_no;
 
--- 4. For each employee, list: employee number, last name, first name, department name
-SELECT e.emp_no AS "Employee No.",
-	   e.last_name AS "Last Name",
-	   e.first_name AS "First Name",(
-		   SELECT dept_name AS "Department Name"
-	   	   FROM departments AS d
-	   	   WHERE d.dept_no IN (
-		   		SELECT dept_no
-			   	FROM dept_emp AS de
-			  	WHERE de.employee_no = e.emp_no
-					  	    	)
-	   )
-FROM employees AS e;
-
-
+--4. For each employee, list: employee number, last name, first name, department name
+-- Creating a view to answer questions no. 6 and 7
+CREATE VIEW emp_dept AS
 SELECT e.emp_no AS "Employee No.",
 	   e.last_name AS "Last Name",
 	   e.first_name AS "First Name",
@@ -52,3 +40,26 @@ RIGHT JOIN dept_emp AS de
 ON e.emp_no = de.employee_no
 RIGHT JOIN departments as d
 ON de.dept_no = d.dept_no;
+
+--5. List first name, last name, sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT *
+FROM emp_dept
+WHERE "Department Name" = 'Sales';
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT *
+FROM emp_dept
+WHERE "Department Name" = 'Sales'
+OR "Department Name" = 'Development';
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS "Frequency"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency" DESC;
--- EmployeeSQL/Schema.sql
+++ EmployeeSQL/Schema.sql
@@ -2,6 +2,7 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/KkcN6U
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+-- CSV files were imported in the following order: titles; departments; employees; salaries; dept_managers; dept_employees 
 
 CREATE TABLE "departments" (
     "dept_no" VARCHAR   NOT NULL,
@@ -11,12 +12,12 @@ CREATE TABLE "departments" (
      )
 );
 
+
 CREATE TABLE "dept_emp" (
     "employee_no" INT   NOT NULL,
     "dept_no" VARCHAR   NOT NULL,
-    "(employee_no," dept_no)   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "(employee_no,"
+        "employee_no", "dept_no"
      )
 );
 
@@ -28,14 +29,15 @@ CREATE TABLE "dept_manager" (
      )
 );
 
+-- Original data type (Date) caused csv importing problem, so changed it to VARCHAR in sql script
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
     "emp_title_id" VARCHAR   NOT NULL,
-    "birth_date" DATE   NOT NULL,
+    "birth_date" VARCHAR   NOT NULL,
     "first_name" VARCHAR   NOT NULL,
     "last_name" VARCHAR   NOT NULL,
     "sex" VARCHAR   NOT NULL,
-    "hire_date" DATE   NOT NULL,
+    "hire_date" VARCHAR   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
@@ -60,6 +62,7 @@ CREATE TABLE "titles" (
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_employee_no" FOREIGN KEY("employee_no")
 REFERENCES "employees" ("emp_no");
 
+
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
 
@@ -74,4 +77,6 @@ REFERENCES "titles" ("title_id");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
+
 
